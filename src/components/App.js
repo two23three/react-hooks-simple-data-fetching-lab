bd782324c26398faa286219d2ec660693338faca
Tulley Mwenda <Tulleyteyez@gmail.com>
2024-05-02 10:57:30 +0300

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,18 @@
+import React,{useState,useEffect} from "react"
+
 // create your App component here
+function App (){
+const [dogPicture,setDogPicture] = useState(null)
+    useEffect(() => {
+        fetch("https://dog.ceo/api/breeds/image/random")
+        .then(response => response.json())
+        .then((data)=>{
+            setDogPicture(data.message)
+        })
+    },[])
+
+    if (!dogPicture) {
+        return <p>Loading...</p>}
+    return <img src={dogPicture} alt="A Random Dog" />
+}
+export default App
